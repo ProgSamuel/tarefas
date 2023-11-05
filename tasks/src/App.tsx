d47af382b5850c4e31b81351132e5d3f0c63d67e
ProgSamuel <progsamu@gmail.com>
2023-11-05 11:48:47 -0300

    capture working

--- tasks/src/App.tsx
+++ tasks/src/App.tsx
@@ -1,15 +1,20 @@
+import React, { useRef } from 'react';
 import { ButtonStyled } from './Components/Button';
-import { useRef } from 'react';
-import saveTask from './functions/save';
 
-export const taskRef = useRef<HTMLInputElement | null>(null); 
 function App() {
+  const taskRef = useRef<HTMLInputElement | null>(null);
+
+  function saveTask(e: React.FormEvent) {
+    e.preventDefault();
+    const task = taskRef.current?.value;
+    console.log(task);
+  }
 
   return (
     <>
       <form onSubmit={saveTask}>
         <input type="text" placeholder='Add task' ref={taskRef} />
-        <ButtonStyled type='submit'> Add </ButtonStyled>
+        <ButtonStyled type="submit">Add</ButtonStyled>
       </form>
     </>
   );
