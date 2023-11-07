4e2707e82f23ed0359001915e96e113c75888345
ProgSamuel <progsamu@gmail.com>
2023-11-07 16:51:21 -0300

    v2

--- tasks/src/App.tsx
+++ tasks/src/App.tsx
@@ -5,7 +5,6 @@ import React, {
   useCallback,
   useMemo,
 } from 'react';
-import { ButtonStyled } from './Components/Button';
 import { v4 as uuidv4 } from 'uuid';
 
 function App() {
@@ -13,7 +12,9 @@ function App() {
   const [tasks, setTasks] = useState<
     { id: string; text: string; completed: boolean }[]
   >([]);
+  const [editingTask, setEditingTask] = useState<{id:String, text:string, completed:boolean}|null>(null);
   const [filter, setFilter] = useState<'all' | 'completed' | 'pending'>('all');
+  const [visible, setVisible] = useState<number>(5);
 
   useEffect(() => {
     const tasksFromLocalStorage: {
@@ -36,15 +37,21 @@ function App() {
   const saveTask = useCallback((e: React.FormEvent) => {
     e.preventDefault();
     const taskText = taskInputRef.current?.value || '';
-    const taskId = uuidv4();
-
-    setTasks((prevTasks) => [
-      ...prevTasks,
-      { id: taskId, text: taskText, completed: false },
-    ]);
+    const taskId = editingTask ? editingTask.id : uuidv4();
+  
+    if (editingTask) {
+      setTasks(prevTasks =>
+        prevTasks.map(task => (task.id === editingTask.id ? { ...task, text: taskText } : task))
+      );
+      setEditingTask(null); 
+    } else {
+      setTasks(prevTasks => [...prevTasks, { id: taskId, text: taskText, completed: false }]);
+    }
+  
     localStorage.setItem(taskId, taskText);
     taskInputRef.current!.value = '';
-  }, []);
+  }, [editingTask]);
+  
 
   const toggleTaskCompletion = useCallback((taskId: string) => {
     setTasks((prevTasks) => {
@@ -72,33 +79,45 @@ function App() {
     localStorage.removeItem(id);
     setTasks((prevItems) => prevItems.filter((item) => item.id !== id));
   }
-  // function editTask(task:any){
-  //   localStorage.getItem(task.id)
-  // }
+  function editTask(task: any) {
+setEditingTask({id: task.id, text: task.text, completed: task.completed})    
+    taskInputRef.current!.value = task.text;  }
 
+    const toggleVisibleTasks = () => {
+      setVisible((prevVisible) => (prevVisible === filterTasks.length ? 5 : filterTasks.length));
+    };
   return (
-    <>
+    <section>
+      <h1> To-do List </h1>
       <form onSubmit={saveTask}>
         <input type="text" placeholder="Add task" ref={taskInputRef} required />
-        <ButtonStyled type="submit">Add</ButtonStyled>
+        <button type="submit"> {editingTask? 'Update': 'Add'}</button>
       </form>
       <div className="buttons">
-        <ButtonStyled onClick={() => setFilter('all')}>Show All</ButtonStyled>
-        <ButtonStyled onClick={() => setFilter('completed')}>
+        <button onClick={() => setFilter('all')} style={{
+          backgroundColor: filter==='all' ? 'orange ':'black'
+        }}>Show All</button>
+        <button onClick={() => setFilter('completed')}
+        style={{
+          backgroundColor: filter==='completed' ? 'orange ':'black'
+        }}
+        >
           Completed
-        </ButtonStyled>
-        <ButtonStyled onClick={() => setFilter('pending')}>
+        </button>
+        <button onClick={() => setFilter('pending')} style={{
+          backgroundColor: filter==='pending' ? 'orange ':'black'
+        }}>
           Pending
-        </ButtonStyled>
+        </button>
       </div>
       <ul>
-        {filterTasks.map((task) => (
+        {filterTasks.slice(0,visible).map((task) => (
           <li
             key={task.id}
             onClick={() => toggleTaskCompletion(task.id)}
             style={{
               textDecoration: task.completed ? 'line-through' : 'none',
-              backgroundColor: task.completed ? '#C1E1C1' : '#F4AFAF',
+              backgroundColor: task.completed ? '#98FB98' : '#FFB6C1',
             }}
           >
             {`${task.text}`}
@@ -106,14 +125,19 @@ function App() {
               <button onClick={() => removeTask(task.id)}>
                 <i className="fa-solid fa-trash"></i>
               </button>
-              {/* <button onClick={() => editTask(task)}>
+              <button onClick={() => editTask(task)}>
                 <i className="fa-solid fa-pen-to-square"></i>
-              </button> */}
+              </button>
             </div>
           </li>
         ))}
+      {filterTasks.length >= visible && (
+          <li className='seeMore' onClick={toggleVisibleTasks}>
+            {visible === filterTasks.length ? 'See Less' : 'See More'}
+          </li>
+        )}
       </ul>
-    </>
+    </section>
   );
 }
 
